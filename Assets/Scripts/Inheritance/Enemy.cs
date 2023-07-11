2e30f4c35bd322d4e7d704a0cfaa53a662153270
albertgalicinskiy <galicinskiyalba@mail.ru>
2023-07-11 15:11:39 +0500

    Inheritance in C# - Part 1

---
+++ Assets/Scripts/Inheritance/Enemy.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy
+{
+    public void Attack()
+    {
+        Debug.Log("Enemy Atack");
+    }
+}
