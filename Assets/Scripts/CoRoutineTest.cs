d021257cc40cfd5cdaabb595429dce6f134f102a
albertgalicinskiy <galicinskiyalba@mail.ru>
2023-07-12 19:53:24 +0500

    Understanding Coroutines in Unity C#

--- Assets/Scripts/CoRoutineTest.cs
+++ Assets/Scripts/CoRoutineTest.cs
@@ -10,16 +10,35 @@ public class CoRoutineTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       //  StartCoroutine(Test());
-        StartCoroutine("Test"); // the most recommended way
+        // StartCoroutine(Test());
+        // StartCoroutine("Test"); // the most recommended way
+
+        // Test2();
+        // // only after finishing Test2()
+        // for (int i = 0; i < 6; i++ )
+        // {
+        //     print("Start()");
+        // }
+
+
+        // will be running in parallel
+        // CoRoutine without parameters
+        // StartCoroutine("Test3");
+
+        // CoRoutine with parameters
+        StartCoroutine("Test3", 8);
+
+        // not waiting StartCoroutine("Test3");
+        // for (int i = 0; i < 6; i++ )
+        // {
+        //     print("Start()");
+        // }
+
+        // StopCoroutine("Test3");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
+    // CoRoutines run in parallel to your current code
     // CoRoutine
     IEnumerator Test()
     {
@@ -34,7 +53,28 @@ public class CoRoutineTest : MonoBehaviour
 
         GetComponent<Renderer>().material.color = Color.green;
 
+        // we have to wait for current frame to be finished and new frame to be started
+        yield return null; 
+
         print(" CoRoutine Ends");
+    }
+
+    void Test2()
+    {
+        for (int i = 0; i < 6; i++ )
+        {
+            print("Test2()");
+        }
+    }
+
+    IEnumerator Test3(int times)
+    {
+        for (int i = 0; i < times; i++ )
+        {
+            print("Test3()");
+
+            yield return null;
+        }
     }
 
 }
